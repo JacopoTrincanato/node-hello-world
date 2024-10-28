19f5756ced1c651a4a5590dc8632f5d7dd0802fb
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-28 17:48:06 +0100

    solve undefined bugs

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 //creo un file main.js con un semplice console.log che dica hello node js
 console.log('hello node js');
 
@@ -7,7 +5,7 @@ console.log('hello node js');
 const pin = process.env.PIN;
 console.log(pin);
 
-const newPin = process.argv[2];
+const newPin = process.argv.slice(2);
 console.log(newPin);
 
 
