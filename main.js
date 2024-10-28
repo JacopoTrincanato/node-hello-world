a719d4b8c5cfeaffd8347fce406183c707c621ac
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-28 15:48:02 +0100

    try to add a variable

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 /*Creiamo la prima applicazione con NodeJs:
-Iniziate creando un file main.js con un semplice console.log che dica hello node js
-fate girare lo script usando node main.js
-aggiungete allo script una variabile di ambiente chiamata PIN  con associato numero a piacere es. PIN=666
 usate il modulo process per recuperare e stampare in console dalla proprietá env la variabile PIN
 ora passate un argomento al vostro script e recuperatene il valore usando process e la proprietá argv per stamparlo in console
 Bonus:
@@ -10,3 +7,9 @@ altrimenti stampa Access restricted*/
 
 //creo un file main.js con un semplice console.log che dica hello node js
 console.log('hello node js');
+
+//aggiungete allo script una variabile di ambiente chiamata PIN  con associato numero a piacere es. PIN=666
+const newPin = process.env.API_KEY
+
+console.log(newPin);
+
