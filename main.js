c7a349b865faeedc261026846f8a4565486cbde5
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-29 10:02:30 +0100

    fix few bugs

--- main.js
+++ main.js
@@ -2,17 +2,17 @@
 console.log('hello node js');
 
 //aggiungo allo script una variabile di ambiente chiamata PIN  con associato numero a piacere
-const pin = process.env.PIN;
+const pin = process.env.pin;
 console.log(pin);
 
 //passo un argomento al vostro script e recupero il valore usando process e la proprietá argv per stamparlo in console
-const newPin = process.argv.slice(2);
+const newPin = process.argv[2]
 console.log(newPin);
 
 /*Bonus:
 se il valore dell'argomento é admin ed il valore di PIN definito é 666 allora stampa in console Welcome Admin
 altrimenti stampa Access restricted*/
-if ((Number(pin) === 666) && (process.argv.slice(2) === 'admin')) {
+if ((pin == 666) && (newPin == 'admin')) {
     console.log('Welcome admin');
     
 }else{
